842c85e2f5f372884731ac592a1c88127bd02377
Madhu <madhuravius@protonmail.com>
2022-08-16 08:18:35 -0400

    fix: fixed top level click funcs and usages

--- nn/cli.py
+++ nn/cli.py
@@ -2,7 +2,6 @@ from os import get_terminal_size
 from typing import Any, Optional
 
 import rich_click as click
-from click_default_group import DefaultGroup
 from rich import print
 from rich.console import Console
 
@@ -10,12 +9,12 @@ from .articles.service import get_and_show_articles
 from .util import click_async
 
 
-@click.group(cls=DefaultGroup, default="download", default_if_no_args=True)
+@click.group()
 def app() -> Optional[Any]:
     pass
 
 
-@click.command()  # type: ignore
+@click.command("all")  # type: ignore
 @click.option(
     "--debug",
     "-d",
@@ -33,9 +32,9 @@ def app() -> Optional[Any]:
     data. This is based around a 1-index.""",
 )
 @click_async
-async def cli(debug: bool, page: int) -> None:
+async def list_articles(debug: bool, page: int) -> None:
     """
-    This CLI consolidates news entries by popular news sources and de-duplicates them when a common
+    This consolidates all news entries by popular news sources and de-duplicates them when a common
     source is encountered (ex: HackerNews, Reddit, Lobste.rs, etc.)
     """
     if debug:
@@ -46,6 +45,9 @@ async def cli(debug: bool, page: int) -> None:
     results = await get_and_show_articles(debug=debug, page=page, page_size=page_size)
     console.print(results)
     return
+
+
+app.add_command(list_articles)
 
 
 if __name__ == "__main__":
