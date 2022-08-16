ff2d0f4d46b43a64e83a693c8234accd430133c2
Madhu <madhuravius@protonmail.com>
2022-08-16 00:18:41 -0400

    fix: initial fix to builds

--- nn/articles/models.py
+++ nn/articles/models.py
@@ -1,5 +1,5 @@
 import uuid
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import List, Optional
 
 from dataclasses_json import dataclass_json
@@ -8,10 +8,10 @@ from dataclasses_json import dataclass_json
 @dataclass_json
 @dataclass
 class ArticleDataSource:
-    hn_by: Optional[str]
-    hn_descendants: Optional[int]
-    hn_id: Optional[int]
-    hn_score: Optional[int]
+    hn_by: Optional[str] = None
+    hn_descendants: Optional[int] = None
+    hn_id: Optional[int] = None
+    hn_score: Optional[int] = None
 
 
 @dataclass_json
@@ -24,4 +24,4 @@ class Article:
     score: float
     created_date: str
     modified_date: str
-    article_data_sources: List[ArticleDataSource]
+    article_data_sources: list[ArticleDataSource]
